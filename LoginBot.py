db4a48d12d39548713f5679c3e37b4e6be098331
florinczi <140913385+florinczi@users.noreply.github.com>
2023-10-03 18:46:07 +0200

    Add files via upload

--- LoginBot.py
+++ LoginBot.py
@@ -1,12 +1,12 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
-#from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 driver.get('https://www.saucedemo.com')
 driver.implicitly_wait(8)
