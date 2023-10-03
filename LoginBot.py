a6b1a42743192b79a8cb5b0e5041f7746875575e
florinczi <140913385+florinczi@users.noreply.github.com>
2023-10-03 18:44:26 +0200

    Add files via upload

---
+++ LoginBot.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+#from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
+
+
+driver = webdriver.Chrome()
+
+driver.get('https://www.saucedemo.com')
+driver.implicitly_wait(8)
+usr = driver.find_element(By.ID, 'user-name')
+pwd = driver.find_element(By.ID, 'password')
+loginButton = driver.find_element(By.ID, 'login-button')
+
+usr.send_keys("standard_user")
+pwd.send_keys("secret_sauce")
+loginButton.click()
+
+# print (WebDriverWait(driver, 30).until(
+#         EC.text_to_be_present_in_element(
+#         (By.CLASS_NAME, 'title'), #Szukanie elementu
+#         'Complete!'
+#     )
+# ))
+
+
+loginConfirmedElement = driver.find_element(By.CLASS_NAME, 'title')
+if loginConfirmedElement.text == 'Products':
+    print ("Test successful.")
+
+
